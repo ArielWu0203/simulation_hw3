3673ef022d66857d9e11ed6d0b60c95e189876fd
ArielWu0203 <lucky020372@gmail.com>
2019-11-29 14:53:15 +0800

    Finish Q2,Q3 2,3

--- MM1.py
+++ MM1.py
@@ -47,12 +47,28 @@ def simulation(customer_num ,arrival_rate, service_rate):
             next_time = next_time+service_time[index]
             depart_time.append(next_time)
 
-    print("Arrival rate",1/mean(inter_arrival))
-    print("Service rate",1/mean(service_time))
-    print("Utiliztion", sum(service_time)/next_time)
-    print("Average in line", mean(queuing_time))
+    # TODO : calculate # of peaple in system & line
+    arr_sys = [0 for n in range(int(depart_time[customer_num-1])+1) ]
+    arr_line = [0 for n in range(int(depart_time[customer_num-1])+1) ]
+
+    for index, value in enumerate(arrive_time):
+        at_time = int(value)
+        leave_time = int(depart_time[index])
+        serve_time =  int(value+queuing_time[index])
+
+        for t in range(at_time, leave_time):
+            arr_sys[t]+=1
+        for t in range(at_time, serve_time):
+            arr_line[t]+=1
+
+    print("1. Arrival rate : ",1/mean(inter_arrival))
+    print("2. Service rate : ",1/mean(service_time))
+    print("3. Utiliztion : ", sum(service_time)/next_time)
+    print("4. Average # of customers in line : ", mean(arr_line))
+    print("5. Average # of customers in system : ", mean(arr_sys))
+    print("6. Average in line : ", mean(queuing_time))
     waiting_time = [depart_time[i]-arrive_time[i] for i in range(0,customer_num)]
-    print("Average in system", mean(waiting_time))
+    print("7. Average in system : ", mean(waiting_time))
 
 if __name__ == '__main__' :
     try:
